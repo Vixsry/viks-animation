39393a1fb9fb53d7401fbdbaac8f2cb1113d89ba
Vixsry <kingvikvik25@gmail.com>
2025-01-02 17:06:22 +0700

    Update build-css.js

--- build-css.js
+++ build-css.js
@@ -1,21 +1,19 @@
 const fs = require('fs');
 const path = require('path');
-const postcss = require('postcss');
 const cssnano = require('cssnano');
-const postcssBanner = require('postcss-banner');
-
-// Baca file CSS sumber
+const postcss = require('postcss');
+// PUNGSI MEMBACA FILE SUMBER
 const inputCss = fs.readFileSync(path.join(__dirname, 'src/viks.css'), 'utf8');
-
-// Proses dan minifikasi CSS
-processor.process(inputCss, { from: undefined })
-  .then(result => {
-    // Tulis hasil ke file output dengan lisensi di bagian atas
-    fs.writeFileSync(path.join(__dirname, 'dist/viks.min.css'), result.css);
-    if (result.map) {
-      fs.writeFileSync(path.join(__dirname, 'dist/viks.min.css.map'), result.map);
-    }
-  })
-  .catch(error => {
-    console.error('Error during CSS processing:', error);
-  });
+// CSS NANO UNTUK PROSES MINIFIKASI CSS
+postcss([cssnano])
+.process(inputCss, { from: undefined })
+.then(result => {
+// Tulis hasil minifikasi ke file output
+fs.writeFileSync(path.join(__dirname, 'dist/viks.min.css'), result.css);
+if (result.map) {
+fs.writeFileSync(path.join(__dirname, 'dist/viks.min.css.map'), result.map);
+}
+})
+.catch(error => {
+console.error('Error during CSS minification:', error);
+});
