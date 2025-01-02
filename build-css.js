7f38fc6f74ffd9a2d7f6eeb8644e84f65d958618
Vixsry <kingvikvik25@gmail.com>
2025-01-02 17:02:13 +0700

    Update build-css.js

--- build-css.js
+++ build-css.js
@@ -7,24 +7,6 @@ const postcssBanner = require('postcss-banner');
 // Baca file CSS sumber
 const inputCss = fs.readFileSync(path.join(__dirname, 'src/viks.css'), 'utf8');
 
-// Konfigurasi PostCSS dengan banner dan cssnano
-const processor = postcss([
-  postcssBanner({
-    banner: `/*!
-     * MIT LISENSI
-     * Copyright (c) 27 Nov 2024 Vixsry
-     * Lisensi   : https://github.com/Vixsry/viks-animation/blob/main/LICENSE
-     * Website   : https://viksanimation.my.id/
-     * Github    : https://github.com/Vixsry/viks-animation
-     * Author    : VIKRI AHPAD TANTOWI
-     * Instagram : https://www.instagram.com/viksry12
-     * Facebook  : https://www.facebook.com/share/1E17jqYu34/
-     */`,
-    important: true
-  }),
-  cssnano()
-]);
-
 // Proses dan minifikasi CSS
 processor.process(inputCss, { from: undefined })
   .then(result => {
